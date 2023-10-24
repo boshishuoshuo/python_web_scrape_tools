ea2f3acef1a3eb9ae555273cf72e287c7b58d750
Yan Feng <fengyan0121@gmail.com>
2023-10-23 23:36:08 -0400

    Start open web page with python

---
+++ open_web_page/launch_map.py
@@ -0,0 +1,13 @@
+#! python
+
+'''
+launch_map.py - launches a map in the browser using an address
+from the command line or clipboard. 
+'''
+
+import webbrowser, sys
+
+if len(sys.argv) > 1:
+    address = ' '.join(sys.argv[1:])
+
+webbrowser.open(address)
