60ab6a20fe45fafd374837f825fb3f1b1bb58ce0
Yan Feng <fengyan0121@gmail.com>
2023-10-25 23:30:07 -0400

    Launch a google map from command line or clipboard

--- open_web_page/launch_map.py
+++ open_web_page/launch_map.py
@@ -5,9 +5,12 @@ launch_map.py - launches a map in the browser using an address
 from the command line or clipboard. 
 '''
 
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 
 if len(sys.argv) > 1:
     address = ' '.join(sys.argv[1:])
+else:
+    #get address from clipboard
+    address = pyperclip.paste()
 
-webbrowser.open(address)
+webbrowser.open('https://www.google.com/maps/place/' + address)
