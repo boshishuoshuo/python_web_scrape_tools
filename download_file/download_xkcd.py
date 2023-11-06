ce5108bd80b1eb2fdb59a344ed931cf66705cd9a
Yan Feng <fengyan0121@gmail.com>
2023-11-05 22:48:20 -0500

    finish download comic script

--- download_file/download_xkcd.py
+++ download_file/download_xkcd.py
@@ -8,15 +8,32 @@ url = 'https://xkcd.com/'
 os.makedirs('xkcd', exist_ok=True)
 
 while not url.endswith('#'):
-    # TODO: Download the page.
+    # Download the page.
+    print('Downloading page %s ...' % url)
+    res = requests.get(url)
+    res.raise_for_status()
+
+    soup = bs4.BeautifulSoup(res.text, 'html.parser')
+    # Find the URL of the comic image.
+    comicElem = soup.select('#comic img')
+    if comicElem == []:
+        print('Could not find comic image.')
+    else:
+        comicUrl = comicElem[0].get('src')
+        # Download the image.
+        print('Downloading image %s ...' % (comicUrl))
+        res = requests.get('https:' + comicUrl)
+        res.raise_for_status()
     
-    # TODO: Find the URL of the comic image.
+    # Save the image to ./xkcd
+    with open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb') as imageFile:
+        for chunk in res.iter_content(100000):
+            imageFile.write(chunk)
     
-    # TODO: Download the image.
+    # Get the Prev button's url.
+    prevLink = soup.select('a[rel="prev"]')
+    url = 'https://xkcd.com/'+prevLink[0].get('href')
     
-    # TODO: Save the image to ./xkcd
-    
-    # TODO: Get the Prev button's url.
-    break
+    print(url)
 
 print('Done.')
